ff33461508bb550f51fd3c6358b79b0932df6413
aaaabima <aaaabim@gmail.com>
2022-12-09 06:23:55 +0700

    finished project with logistic regression

---
+++ TestingMlNet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestingMlNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
